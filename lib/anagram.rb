48835b2cda6d6fe62ef7516c0d7bed980e99ee81
Seth Near <seth.j.near@gmail.com>
2020-06-24 02:40:32 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,8 +8,18 @@ def initialize(word)
   @word = word
 end
 
+def match(array)
+matches = []
+array.each {|x| 
+    if alphabetize(x) == alphabetize(@word)
+      matches << x
+    end
+ }
 
+end
 
-
-
+def alphabetize(string)
+  letters = string.split("")
+  letters.sort.join
+end
 end
